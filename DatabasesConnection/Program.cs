62b497b23b6c4b1c58ba67a8945b8fabb40fb2c6
J ochoa <jochoa8a@msn.com>
2017-11-08 22:41:51 +0100

    Add project files.

---
+++ DatabasesConnection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomerManager
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            System.Console.WriteLine("Starting program <<<<<<<<<<<<<<<<<<<<<<<<<<");
+            // The task: Create a customer manager program with queues etc similar to lantec
+            /*
+             1. Connect to any database
+             2. Create db basic schema
+
+            */
+            int test = 12;
+            Application.Run(new MainWindow());
+        }
+    }
+}
